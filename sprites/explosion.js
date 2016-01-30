0da6e58b1fa936448d9fe50ec43b41eea331729e
Dustin Kane <dustinpkane@gmail.com>
2016-01-29 18:31:45 -0800

    Made an explosion pretty much

--- sprites/explosion.js
+++ sprites/explosion.js
@@ -1,40 +1,163 @@
 "use strict";
 
 
-var explosion = function ( id, x, y, scale) {
-    var img = new Image();
-    img.src = "explosion.png";
+var explosion = function ( id, props) {
 
     var explosionObject = {
         "canvas": document.getElementById(id),
-        MAXSTEP: 400,
-        stepval: 0,
-        step: function(){
-            console.log(this.stepval);
-            if(++this.stepval === this.MAXSTEP){
-                this.stepval = 0;
-            }
-        },
-        draw: function () {
+        MAXSTEP: props.maxstep !== undefined ? props.maxstep : 100,
+        x: props.x !== undefined ? props.x : 0,
+        y: props.y !== undefined ? props.x : 0,
+        width: props.width !== undefined ? props.width : 300,
+        height: props.height !== undefined ? props.height : 300,
+
+        draw: function (step) {
             var ctx = this.canvas.getContext( "2d" );
 
             ctx.save();
 
             ctx.clearRect( 0, 0, this.canvas.width, this.canvas.height ); // clear canvas
 
-            var w = 300,
-                h = 300;
+            var w = this.width * step / this.MAXSTEP,
+                h = this.width * step / this.MAXSTEP;
 
-            ctx.drawImage(img, 0, 0);
-            ctx.scale(this.stepval/this.MAXSTEP , this.stepval/this.MAXSTEP);
+            ctx.fillStyle = "rgb(0, 255, 255)";
 
-            console.log(this.stepval);
-            console.log(this.stepval/this.MAXSTEP);
+            ctx.fillRect(this.x, this.y, w, h);
+
+            var drawExplosion = function(ctx, x, y, w, h, color) {
+                ctx.beginPath();
+                // 10x, 10y
+                // -5, -5
+                x += w * -0.5;
+                y += h * -0.5;
+                ctx.moveTo(x, y);
+
+                // 0, -2
+                x += w * 0.4;
+                y += h * 0.3;
+                ctx.lineTo(x, y);
+
+                // 1, -4
+                x += w * 0.1;
+                y += h * -0.2;
+                ctx.lineTo(x, y);
+
+                // 2, -2
+                x += w * 0.1;
+                y += h * 0.2;
+                ctx.lineTo(x, y);
+
+                // 4, -4.5
+                x += w * 0.3;
+                y += h * -0.25;
+                ctx.lineTo(x, y);
+
+                // 2, -1.5
+                x += w * -0.2;
+                y += h * 0.3;
+                ctx.lineTo(x, y);
+
+                // 4, -2
+                x += w * 0.2;
+                y += h * -0.05;
+                ctx.lineTo(x, y);
+
+                // 2, 0
+                x += w * -0.2;
+                y += h * 0.2;
+                ctx.lineTo(x, y);
+
+                // 5, 1
+                x += w * 0.3;
+                y += h * 0.1;
+                ctx.lineTo(x, y);
+
+                // 2, 1
+                x += w * -0.3;
+                y += h * 0;
+                ctx.lineTo(x, y);
+
+                // 5, 3
+                x += w * 0.3;
+                y += h * 0.2;
+                ctx.lineTo(x, y);
+
+                // 1.5, 2
+                x += w * -0.35;
+                y += h * -0.1;
+                ctx.lineTo(x, y);
+
+                // 1, 4
+                x += w * -0.05;
+                y += h * 0.2;
+                ctx.lineTo(x, y);
+
+                // 0, 2.5
+                x += w * -0.1;
+                y += h * -0.15;
+                ctx.lineTo(x, y);
+
+                // -2, 5
+                x += w * -0.2;
+                y += h * 0.25;
+                ctx.lineTo(x, y);
+
+                // -1.5, 2.5
+                x += w * 0.05;
+                y += h * -0.25;
+                ctx.lineTo(x, y);
+
+                // -4.5, 3
+                x += w * -0.3;
+                y += h * 0.05;
+                ctx.lineTo(x, y);
+
+                // -2.5, 0.5
+                x += w * 0.2;
+                y += h * -0.25;
+                ctx.lineTo(x, y);
+
+                // -5, -2
+                x += w * -0.25;
+                y += h * -0.25;
+                ctx.lineTo(x, y);
+
+                // -2.5, -1.5
+                x += w * 0.25;
+                y += h * 0.05;
+                ctx.lineTo(x, y);
+
+
+                ctx.closePath();
+
+                ctx.fillStyle = color;
+                ctx.fill();
+            };
+
+
+            ctx.save();
+            ctx.translate(this.x + w/2, this.y +  h/2);
+            drawExplosion(ctx, 0, 0, w, h, "rgb(255, 0, 0)");
+            ctx.restore();
+
+            ctx.save();
+            ctx.translate(this.x + w/2, this.y +  h/2);
+            drawExplosion(ctx, 0, 0, w * 0.75, h * 0.75, "rgb(255, 153, 0)");
+            ctx.restore();
+
+
+            ctx.save();
+            ctx.translate(this.x + w/2, this.y +  h/2);
+            drawExplosion(ctx, 0, 0, w * 0.5, h * 0.5, "rgb(255, 255, 0)");
+            ctx.restore();
+
+
 
             ctx.restore();
         }
     };
 
-    explosionObject.draw();
+    explosionObject.draw(150);
     return explosionObject;
 };
